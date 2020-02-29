44548054440feeb400ae42723150122196d8a3a4
chakradhar1140 <61564121+chakradhar1140@users.noreply.github.com>
2020-02-28 19:56:05 -0800

    Update variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,29 @@
+variable "AWS_ACCESS_KEY" {
+   description = My AWS AccessKey
+   default = "AKIA24ZM67STXQMJWXGJ"
+
+}
+
+variable "AWS_SECRET_KEY" {
+   description = My AWS Secret AccessKey
+   default = "cP2ubwSrmGX9XoeeOyl5dZFes+HHpsb+HZ3oc4sA"
+
+}
+
+variable "AMI" {
+   description = My CUSTOM AMI
+   default = "ami-0520e698dd500b1d1"
+
+}
+
+variable "AWS_REGION" {
+   description = Default Region
+   default = "us-east-2"
+
+}
+
+variable "key_name" {
+   description = My Keypair
+   default =  "linuxkp"
+
+}
